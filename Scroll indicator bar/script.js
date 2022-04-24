183655b109f3e3602deb5ce93fa1fe82630f3c7b
TDMWalpala <tharindudarshana2019@gmail.com>
2022-04-24 10:05:25 +0530

    Scroll Indicator Bar

---
+++ Scroll indicator bar/script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("scroll", () => {
+    const indicatorBar = document.querySelector(".scroll__indicator__bar");
+
+    const pageScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollValue = (pageScroll / height) * 100;
+
+    indicatorBar.style.width = scrollValue + "%";
+  });
